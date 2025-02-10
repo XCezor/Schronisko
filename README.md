7253d9062fdc6134c008e3d0775d1f4c189a8a07
Kamil Dolkowski <kamil.dolkowski@op.pl>
2025-02-10 16:11:38 +0100

    Podstawowe pliki dla projektu.

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# Schronisko
+# Poradnik - jak uruchomić stronę?
+
+## Potrzebne na start:
+
+### Baza danych:
+
+- baza o nazwie `html` (w PostgreSQL)
+
+- rola (użytkownik) `admin` z hasłem `admin` z uprawnieniami do bazy, tabel
+
+### Virtual Environment:
+
+- w terminalu: `pip3 install virtualenv`
+
+## Do uruchomienia strony:
+
+- W terminalu (będąc w katalogu repozytorium) wpisz `source env/bin/activate`, to polecenie aktywuje wirtualne środowisko (virtual environment).
+
+- Następnie trzeba uruchomić program app.py, by to zrobić wpisz w terminalu `FLASK_APP=app.py flask run`. Strona internetowa powinna już działać! :D
+
+## Jak zobaczyć stronę?
+
+- By zobaczyć stronę, wpisz do paska wyszukiwania w przeglądarce `127.0.0.1:5000` lub `localhost:5000`. Strona powinna się wyświetlić.
+
+## Co jak chcę wprowadzić zmianę w stronie?
+
+- Aby wprowadzić zmiany do (aktywnej) strony należy najpierw wyłączyć aplikację app.py, by to zrobić, wpisz w terminalu `CTRL+C`.
+
+- Teraz możesz dokonać zmian w plikach. Żeby po zmianach z powrotem zobaczyć efekt, wpisz do terminala `FLASK_APP=app.py flask run`.
+
+## Jak wyłączyć env?
+
+- Po wyłączeniu strony (CTRL+C), wpisz do terminala `deactivate`.
+
+&nbsp;
+
+&nbsp;
+
+# Polecenia w skrócie
+
+### W terminalu:
+
+`source env/bin/activate` - włączenie wirtualnego środowiska (env)
+
+`deactivate` - wyłączenie wirtualnego środowiska (env)
+
+`FLASK_APP=app.py flask run` - uruchomienie app.py (uruchomienie strony)
+
+`CTRL+C` - by wyłączyć app.py
+
+### W pasku wyszukiwania:
+
+`127.0.0.1:5000` lub `localhost:5000` - "domena"
+ strony
---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@localhost/html'
+
+db = SQLAlchemy(app)
+
+class Pets(db.Model):
+    pet_id = db.Column(db.Integer, primary_key=True)
+    type_id = db.Column(db.Integer, db.ForeignKey('types.type_id')) 
+    name = db.Column(db.String(30))
+    breed = db.Column(db.String(30))
+    age = db.Column(db.Integer)
+    description = db.Column(db.String(1000))
+    date_add = db.Column(db.TIMESTAMP, default=datetime.utcnow())
+    date_on = db.Column(db.TIMESTAMP, default=datetime.utcnow())
+    date_off = db.Column(db.TIMESTAMP, default=datetime.utcnow())
+
+    type = db.relationship('Types', backref='pets', lazy=True)
+
+class Types(db.Model):
+    type_id = db.Column(db.Integer, primary_key=True) 
+    name = db.Column(db.String(50))
+    
+
+@app.route("/")
+def hello():
+    db.create_all()
+
+    # pets = Pets.query.all()
+    pets = db.session.query(Pets.name, Pets.age, Types.name.label("type"), Pets.description).join(Types, Types.type_id == Pets.type_id).all()
+
+    return render_template("index.html", animals=pets)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_animal():
+    if request.method == 'POST':
+        new_animal = Pets(name=request.form['name'], age=request.form['age'], type_id=request.form['type_id'], description=request.form['description'])
+        db.session.add(new_animal)
+        db.session.commit()
+
+        return redirect("/")
+
+    pets = Pets.query.all()
+    return render_template("add.html", animals=pets)
+
+
+    
+
+
+# @app.route('/animals')
+# def get_animals():
+#     animals = Animal.query.all()
+#     animals_list = '<br>'.join([f'{animal.name}, {animal.species}' for animal in animals])
+#     return animals_list
+
+# @app.route('/delete/<int:id>', methods=['GET'])
+# def delete_animal(id):
+#     animal_to_delete = Animal.query.get(id)
+    
+#     if animal_to_delete:
+#         db.session.delete(animal_to_delete)  
+#         db.session.commit()
+    
+#     animals = Animal.query.all()
+
+#     return f'Usunięto zwierzę o id={id}'
