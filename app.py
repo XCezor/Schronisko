cc7164e33f5ef0e0e9a7cb38c945e3780dcdf85b
Kamil Dolkowski <kamil.dolkowski@op.pl>
2025-02-16 00:03:44 +0100

    Dodanie 3 ostatnich postów na stronę główną.

--- app.py
+++ app.py
@@ -109,7 +109,34 @@ class LoginForm(FlaskForm):
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    three_latest_posts = Posts.query.order_by(Posts.post_datetime.desc()).filter(Posts.is_deleted == 'FALSE').limit(3)
+    
+    for post in three_latest_posts:
+        # ucinanie opisu posta do 300 znaków (wyświetla tylko 2 pierwsze paragrafy)
+        # (dozwolone tagi: [p, strong, em, s]; dopisywanie brakujących tagów html)
+        original_length = len(bleach.clean(post.description))
+
+        soup = BeautifulSoup(post.description, 'html.parser')
+        paragraphs = soup.find_all('p')
+        first_two_paragraphs = paragraphs[:2]
+
+        post.description = ''
+        number_of_chars = 0
+
+        for p in first_two_paragraphs:
+            if len(post.description + str(p)) > 300:
+                post.description += str(p)[:300-number_of_chars]
+                break
+            post.description += str(p)
+            number_of_chars += len(post.description)
+
+        exit_length = len(bleach.clean(post.description))
+
+        if original_length != exit_length + 1:
+            post.description = post.description[0:-4] + " ...</p>"
+        
+        post.description = bleach.clean(post.description, tags={'p','strong','em','s'}, strip=True)
+    return render_template("home.html", three_latest_posts=three_latest_posts)
 
 # Aktualności - posty
 
